23dcfe2112c55f670c1da7ad841cc80dbb76d728
Thomas <thomaspauldu06@gmail.com>
2018-12-12 14:06:25 +0100

    Ajout de commentaires

--- script2.js
+++ script2.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
 
     $("dt.main-dt").on("click", function() {
+        // Incrémentation du total de clic
         let nbClic = $(this).next()[0].childNodes[1];
         nbClic.innerHTML = parseInt(nbClic.innerHTML) + 1;
+        // Affichage de la réponse cliquée
+        // Disparition des autres réponses
         let actualResponse = $(this).siblings('.response');
-        let otherResponse = $("dt.main-dt").siblings('.response')
-        $(otherResponse).not(actualResponse).slideUp();
-        actualResponse.slideToggle();
+        let allResponse = $("dt.main-dt").siblings('.response');
+        let otherResponse = $(allResponse).not(actualResponse);
+        actualResponse.slideDown();
+        otherResponse.slideUp();
     });
 
 });
